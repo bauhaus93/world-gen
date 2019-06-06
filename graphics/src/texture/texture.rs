7ca8ed79749506e6b016c27ddc69c3440a898602
Jakob <jakobfischer93@gmail.com>
2019-06-06 16:47:26 +0200

    Fixed some texture error handling.

--- graphics/src/texture/texture.rs
+++ graphics/src/texture/texture.rs
@@ -49,10 +49,8 @@ impl Texture {
 impl Drop for Texture {
     fn drop(&mut self) {
         debug!("Deleting texture id = {}", self.texture_id);
-        delete_texture(self.texture_id);
-        match check_opengl_error("gl::DeleteTextures") {
-            Ok(_) => {},
-            Err(e) => error!("{}", e)
+        if let Err(e) = delete_texture(self.texture_id) {
+            error!("{}", e);
         }
     }
 }
@@ -64,14 +62,15 @@ fn create_texture(size: (GLsizei, GLsizei), mipmaps: GLsizei, img_data: &[u8]) -
     unsafe { gl::GenTextures(1, &mut id); }
     check_opengl_error("gl::GenTextures")?;
     debug_assert!(id != 0);
+    
     unsafe { gl::BindTexture(gl::TEXTURE_2D, id); }
-    match check_opengl_error("gl::BindTexture") {
-        Ok(_) => {},
-        Err(e) => {
-            delete_texture(id);
-            return Err(e);
+    if let Err(e) = check_opengl_error("gl::BindTexture") {
+        if let Err(new_err) = delete_texture(id) {
+            error!("Additional error: {}", new_err);
         }
+        return Err(e);
     }
+
     unsafe {
         gl::TexImage2D(
             gl::TEXTURE_2D,
@@ -85,10 +84,18 @@ fn create_texture(size: (GLsizei, GLsizei), mipmaps: GLsizei, img_data: &[u8]) -
             img_data.as_ptr() as * const _
         );
     }
+    if let Err(e) = check_opengl_error("gl::TexImage2D") {
+        if let Err(new_err) = delete_texture(id) {
+            error!("Additional error: {}", new_err);
+        }
+        return Err(e);
+    }
     Ok(id)
 }
 
-fn delete_texture(texture_id: GLuint) {
+fn delete_texture(texture_id: GLuint) -> Result<(), OpenglError> {
     debug_assert!(texture_id != 0);
     unsafe { gl::DeleteTextures(1, &texture_id); }
+    check_opengl_error("gl::DeleteTextures")?;
+    Ok(())
 }

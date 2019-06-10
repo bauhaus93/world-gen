f145ad42a8dd0eae8ceb915aac638ef670769163
Jakob <jakobfischer93@gmail.com>
2019-06-10 18:02:27 +0200

    Commit before merge.

--- world_gen/src/chunk/chunk.rs
+++ world_gen/src/chunk/chunk.rs
@@ -65,7 +65,7 @@ impl Chunk {
             0 => self.mvp * Vector4::new(0., 0., self.height_map.get(&[0, 0]), 1.),
             1 => self.mvp * Vector4::new(size as Float, 0., self.height_map.get(&[size, 0]), 1.),
             2 => self.mvp * Vector4::new(0., size as Float, self.height_map.get(&[0, size]), 1.),
-            3 => self.mvp * Vector4::new(size as Float, size as Float, self.height_map.get(&[size, size]), 1.),
+            3 => self.mvp * Vector4::new(size as Float, size as Float, self.height_map.get(&[size, size]), 1.), 
             _ => unreachable!()
         }
     }
